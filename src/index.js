a79528d6b86abadda290beab372ed66b2f73c3d4
lanzilla <121096653+lanzilla@users.noreply.github.com>
2023-03-16 16:33:49 +1100

    Integrated forecast API

--- src/index.js
+++ src/index.js
@@ -85,26 +85,44 @@ function displayCelsiusTemperature(event) {
   temperatureElement.innerHTML = Math.round(celsiusTemperature);
 }
 
+function formatDay(timestamp) {
+  let date = new Date(timestamp * 1000);
+  let day = date.getDay();
+  let days = ["Sun", "Mon", "Tue", "Wed", "Thu", "Fri", "Sat"];
+
+  return days[day];
+}
+
 function displayForecast(response) {
+  let forecast = response.data.daily;
   let forecastElement = document.querySelector("#forecast");
   let forecastHTML = `<div class="row">`;
-  let days = ["Sat", "Sun", "Mon", "Tue", "Wed"];
-  days.forEach(function (day) {
-    forecastHTML =
-      forecastHTML +
-      `
+  forecast.forEach(function (forecastDay, index) {
+    if (index < 6) {
+      forecastHTML =
+        forecastHTML +
+        `
          <div class="col-2">
-            <div class = "weather-forecast-date">${day}</div>
-            <img src = ""
+            <div class = "weather-forecast-date">${formatDay(
+              forecastDay.dt
+            )}</div>
+            <img src = "http://openweathermap.org/img/wn/${
+              forecastDay.weather[0].icon
+            }@2x.png"
              alt=""
              width=""
              />
             <div class="weather-forecast-temperatures">
-                <span class="weather-forecast-max">30° </span>
-                <span class="weather-forcast-min">22° </span>        
+                <span class="weather-forecast-max">${Math.round(
+                  forecastDay.temp.max
+                )}°</span>
+                <span class="weather-forcast-min">${Math.round(
+                  forecastDay.temp.min
+                )}°</span>        
             </div>
          </div>
    `;
+    }
   });
   forecastHTML = forecastHTML + `</div>`;
   forecastElement.innerHTML = forecastHTML;
